3597339ef1c72a93a1337b60323111fb4eaf1ece
BocuD <duco.born@gmail.com>
2025-04-07 01:34:00 +0200

    Create legacydrawable to render non converted elements using new renderer

---
+++ DTXMania/UI/Drawable/LegacyDrawable.cs
@@ -0,0 +1,23 @@
+﻿using SharpDX;
+
+namespace DTXMania.UI.Drawable;
+
+public class LegacyDrawable : UIDrawable
+{
+    private Action drawAction;
+    
+    public LegacyDrawable(Action drawAction)
+    {
+        this.drawAction = drawAction;
+    }
+    
+    public override void Draw(Matrix parentMatrix)
+    {
+        drawAction();
+    }
+
+    public override void Dispose()
+    {
+        
+    }
+}
