3516a88a420a80ce12b1d1b447da272dd43ea96f
BocuD <duco.born@gmail.com>
2025-04-09 00:58:25 +0200

    Update InspectorManager.cs

--- DTXMania/UI/InspectorManager.cs
+++ DTXMania/UI/InspectorManager.cs
@@ -81,6 +81,7 @@ public static class InspectorManager
         {
             inspector.Draw();
             hierarchyWindow.Draw();
+            DrawableTracker.DrawWindow();
         }
     }
     
