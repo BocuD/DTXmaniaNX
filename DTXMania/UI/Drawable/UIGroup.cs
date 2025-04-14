bf3c310d6c73bfc8c6166237f942e087edb6ee77
BocuD <duco.born@gmail.com>
2025-04-14 19:18:03 +0200

    Properly dispose child elements when clearing children in a group

--- DTXMania/UI/Drawable/UIGroup.cs
+++ DTXMania/UI/Drawable/UIGroup.cs
@@ -62,6 +62,10 @@ public class UIGroup : UIDrawable
         
     public void ClearChildren()
     {
+        foreach (UIDrawable element in children)
+        {
+            element.Dispose();
+        }
         children.Clear();
     }
         
