3bdc98f7a42db3c413dc00d6c4162da0c806f8d1
BocuD <duco.born@gmail.com>
2025-04-14 19:35:31 +0200

    Rework and clean up CActSortSongs.cs

--- DTXMania/Stage/04.SongSelection/CActSortSongs.cs
+++ DTXMania/Stage/04.SongSelection/CActSortSongs.cs
@@ -5,35 +5,88 @@ namespace DTXMania;
 
 internal class CActSortSongs : CActSelectPopupMenu
 {
-
-	// コンストラクタ
-
 	public CActSortSongs()
 	{
 		InitializeMenuItems();
-		
-		tActivatePopupMenu(EInstrumentPart.DRUMS);
 	}
 
+	private List<CItemBase> lci = [];
 	private void InitializeMenuItems()
 	{
-		List<CItemBase> lci = new();
-		lci.Add(new CItemList("Title", CItemBase.EPanelType.Normal, 0, "", "", "Z,Y,X,...", "A,B,C,...")
+		lci = [];
+		
+		CItemList title = new("Title", CItemBase.EPanelType.Normal, 0, "", "", "Z,Y,X,...", "A,B,C,...");
+		title.action = () =>
 		{
-			action = 
-		});
-		lci.Add(new CItemList("Level", CItemBase.EPanelType.Normal, 0, "", "", "99,98,97,...", "1,2,3,..."));
-		lci.Add(new CItemList("Best Rank", CItemBase.EPanelType.Normal, 0, "", "", "E,D,C,...", "SS,S,A,..."));
-		lci.Add(new CItemList("PlayCount", CItemBase.EPanelType.Normal, 0, "", "", "10,9,8,...", "1,2,3,..."));
-		lci.Add(new CItemList("Author", CItemBase.EPanelType.Normal, 0, "", "", "Z,Y,X,...", "A,B,C,..."));
-		lci.Add(new CItemList("SkillPoint", CItemBase.EPanelType.Normal, 0, "", "", "100,99,98,...", "1,2,3,..."));
-		lci.Add(new CItemList("Date", CItemBase.EPanelType.Normal, 0, "", "", "Dec.31,30,...", "Jan.1,2,..."));
-		lci.Add(new CItemList("Return", CItemBase.EPanelType.Normal, 0, "", "", "", ""));
+			int order = title.GetIndex() * 2 - 1;
+			actSongList.tSortSongList(CDTXMania.SongManager.t曲リストのソート2_タイトル順, eInst, order);
+			actSongList.tSelectedSongHasChanged(true);
+		};
+		lci.Add(title);
+
+		CItemList level = new("Level", CItemBase.EPanelType.Normal, 0, "", "", "99,98,97,...", "1,2,3,...");
+		level.action = () =>
+		{
+			int order = level.GetIndex() * 2 - 1;
+			actSongList.tSortSongList(CDTXMania.SongManager.t曲リストのソート4_LEVEL順, eInst, order, actSongList.n現在のアンカ難易度レベル);
+			actSongList.tSelectedSongHasChanged( true );
+		};
+		lci.Add(level);
+
+		CItemList bestRank = new("Best Rank", CItemBase.EPanelType.Normal, 0, "", "", "E,D,C,...", "SS,S,A,...");
+		bestRank.action = () =>
+		{
+			int order = bestRank.GetIndex() * 2 - 1;
+			actSongList.tSortSongList(CDTXMania.SongManager.t曲リストのソート5_BestRank順, eInst, order, actSongList.n現在のアンカ難易度レベル);
+		};
+		lci.Add(bestRank);
+
+		CItemList playCount = new("PlayCount", CItemBase.EPanelType.Normal, 0, "", "", "10,9,8,...", "1,2,3,...");
+		playCount.action = () =>
+		{
+			int order = playCount.GetIndex() * 2 - 1;
+			actSongList.tSortSongList(CDTXMania.SongManager.t曲リストのソート3_演奏回数の多い順, eInst, order, actSongList.n現在のアンカ難易度レベル);
+			actSongList.tSelectedSongHasChanged( true );
+		};
+		lci.Add(playCount);
+
+		CItemList artist = new("Artist", CItemBase.EPanelType.Normal, 0, "", "", "Z,Y,X,...", "A,B,C,...");
+		artist.action = () =>
+		{
+			int order = artist.GetIndex() * 2 - 1;
+			actSongList.tSortSongList(CDTXMania.SongManager.t曲リストのソート8_アーティスト名順, eInst, order, actSongList.n現在のアンカ難易度レベル);
+			actSongList.tSelectedSongHasChanged( true );
+		};
+		lci.Add(artist);
+
+		CItemList? skillPoint = new("SkillPoint", CItemBase.EPanelType.Normal, 0, "", "", "100,99,98,...", "1,2,3,...");
+		skillPoint.action = () =>
+		{
+			int order = skillPoint.GetIndex() * 2 - 1;
+			actSongList.tSortSongList(CDTXMania.SongManager.t曲リストのソート6_SkillPoint順, eInst, order, actSongList.n現在のアンカ難易度レベル);
+			actSongList.tSelectedSongHasChanged( true );
+		};
+		lci.Add(skillPoint);
+
+		CItemList date = new("Date", CItemBase.EPanelType.Normal, 0, "", "", "Dec.31,30,...", "Jan.1,2,...");
+		date.action = () =>
+		{
+			int order = date.GetIndex() * 2 - 1;
+			actSongList.tSortSongList(CDTXMania.SongManager.t曲リストのソート7_更新日時順, eInst, order, actSongList.n現在のアンカ難易度レベル);
+			actSongList.tSelectedSongHasChanged( true );
+		};
+		lci.Add(date);
+
+		CItemList returnButton = new("Return", CItemBase.EPanelType.Normal, 0, "", "", "", "")
+		{
+			action = tDeativatePopupMenu
+		};
+		lci.Add(returnButton);
 
 		Initialize(lci, "SORT MENU", 0);
 	}
 
-	// メソッド
+	private CActSelectSongList actSongList;
 	public void tActivatePopupMenu(EInstrumentPart einst, ref CActSelectSongList ca)
 	{
 		actSongList = ca;
@@ -43,106 +96,9 @@ internal class CActSortSongs : CActSelectPopupMenu
 		base.tActivatePopupMenu(einst);
 	}
 
-	public override void tPressEnterMain( int nSortOrder)  // tEnter押下Main
+	public override void tPressEnterMain()  // tEnter押下Main
 	{
-		nSortOrder *= 2;	// 0,1  => -1, 1
-		nSortOrder -= 1;
-		
-		switch ( nCurrentSelection )
-		{
-			case (int) EOrder.Title:
-				actSongList.tSortSongList(
-					CDTXMania.SongManager.t曲リストのソート2_タイトル順, eInst, nSortOrder
-				);
-				actSongList.tSelectedSongHasChanged(true);
-				break;
-			case (int) EOrder.Level:
-				actSongList.tSortSongList(
-					CDTXMania.SongManager.t曲リストのソート4_LEVEL順, eInst, nSortOrder,
-					actSongList.n現在のアンカ難易度レベル
-				);
-				actSongList.tSelectedSongHasChanged( true );
-				break;
-			case (int) EOrder.BestRank:
-				actSongList.tSortSongList(
-					CDTXMania.SongManager.t曲リストのソート5_BestRank順, eInst, nSortOrder,
-					actSongList.n現在のアンカ難易度レベル
-				);
-				break;
-			case (int) EOrder.PlayCount:
-				actSongList.tSortSongList(
-					CDTXMania.SongManager.t曲リストのソート3_演奏回数の多い順, eInst, nSortOrder,
-					actSongList.n現在のアンカ難易度レベル
-				);
-				actSongList.tSelectedSongHasChanged( true );
-				break;
-			case (int) EOrder.Author:
-				actSongList.tSortSongList(
-					CDTXMania.SongManager.t曲リストのソート8_アーティスト名順, eInst, nSortOrder,
-					actSongList.n現在のアンカ難易度レベル
-				);
-				actSongList.tSelectedSongHasChanged( true );
-				break;
-			case (int) EOrder.SkillPoint:
-				actSongList.tSortSongList(
-					CDTXMania.SongManager.t曲リストのソート6_SkillPoint順, eInst, nSortOrder,
-					actSongList.n現在のアンカ難易度レベル
-				);
-				actSongList.tSelectedSongHasChanged( true );
-				break;
-			case (int) EOrder.Date:
-				actSongList.tSortSongList(
-					CDTXMania.SongManager.t曲リストのソート7_更新日時順, eInst, nSortOrder,
-					actSongList.n現在のアンカ難易度レベル
-				);
-				actSongList.tSelectedSongHasChanged( true );
-				break;
-			case (int) EOrder.Return:
-				tDeativatePopupMenu();
-				break;
-			default:
-				break;
-		}
+		lci[nCurrentSelection].RunAction();
+		lci[nCurrentSelection].RunAction();
 	}
-		
-	// CActivity 実装
-
-	public override void OnDeactivate()
-	{
-		if( !bNotActivated )
-		{
-			base.OnDeactivate();
-		}
-	}
-	public override void OnManagedCreateResources()
-	{
-		if( !bNotActivated )
-		{
-			base.OnManagedCreateResources();
-		}
-	}
-	public override void OnManagedReleaseResources()
-	{
-		base.OnManagedReleaseResources();
-	}
-
-	#region [ private ]
-	//-----------------
-
-	private CActSelectSongList actSongList;  // act曲リスト
-
-	private enum EOrder : int
-	{
-		Title = 0, 
-		Level, 
-		BestRank, 
-		PlayCount,
-		Author,
-		SkillPoint,
-		Date,
-		Return, END,
-		Default = 99
-	};
-	//-----------------
-	#endregion
 }
