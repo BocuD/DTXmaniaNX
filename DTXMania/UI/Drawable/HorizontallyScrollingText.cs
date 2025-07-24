834c5b76090a369bb6d17036ff1da6a79abc0e22
BocuD <duco.born@gmail.com>
2025-07-24 23:53:47 +0200

    Make text scroll speed not framerate dependent

--- DTXMania/UI/Drawable/HorizontallyScrollingText.cs
+++ DTXMania/UI/Drawable/HorizontallyScrollingText.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using DTXMania.Core;
 using Hexa.NET.ImGui;
 using SharpDX;
 using RectangleF = SharpDX.RectangleF;
@@ -9,18 +10,24 @@ public class HorizontallyScrollingText : UIText
 {
     public bool scrollingEnabled;
     public float maximumWidth;
+    public float scrollSpeed = 50.0f;
 
     public HorizontallyScrollingText(FontFamily family, int size) : base(family, size)
     {
         
     }
-
+        
+    private long lastDrawTime;
+    
     public override void Draw(Matrix parentMatrix)
     {
+        float delta = (CDTXMania.Timer.nCurrentTime - lastDrawTime) / 1000.0f;
+        lastDrawTime = CDTXMania.Timer.nCurrentTime;
+        
         //scroll text
         if (scrollingEnabled)
         {
-            overrideClipRect.X += 0.3f;
+            overrideClipRect.X += scrollSpeed * delta;
             if (overrideClipRect.X > texture.Width * 2)
                 overrideClipRect.X = 0;
         }
@@ -56,6 +63,7 @@ public class HorizontallyScrollingText : UIText
         {
             ImGui.Checkbox("Scrolling enabled", ref scrollingEnabled);
             ImGui.InputFloat("Maximum Width", ref maximumWidth);
+            ImGui.InputFloat("Scroll speed", ref scrollSpeed);
         }
     }
 }
