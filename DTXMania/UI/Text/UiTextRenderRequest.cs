eb2d8c1c1305490c570d959d463cb6c4127ad0de
BocuD <duco.born@gmail.com>
2026-04-01 02:31:50 +0900

    Better default parameters for UiTextRenderRequest

--- DTXMania/UI/Text/UiTextRenderRequest.cs
+++ DTXMania/UI/Text/UiTextRenderRequest.cs
@@ -6,23 +6,23 @@ public sealed class UiTextRenderRequest
 {
     public required string Name { get; init; }
     public required string Text { get; init; }
-    public string FontPath { get; init; } = string.Empty;
+    public required string FontPath { get; init; }
     public string FontFamily { get; init; } = string.Empty;
-    public float FontSize { get; init; }
-    public float OutlineWidth { get; init; }
-    public float TexturePadding { get; init; }
-    public float LineSpacing { get; init; }
-    public bool Antialias { get; init; }
-    public bool SubpixelText { get; init; }
-    public UiTextStyle Style { get; init; }
-    public UiTextAlignment Alignment { get; init; }
-    public Color4 FillColor { get; init; }
-    public Color4 OutlineColor { get; init; }
-    public UiTextGradientMode FillGradientMode { get; init; }
-    public Color4 FillGradientTopColor { get; init; }
-    public Color4 FillGradientBottomColor { get; init; }
-    public UiTextGradientMode OutlineGradientMode { get; init; }
-    public Color4 OutlineGradientTopColor { get; init; }
-    public Color4 OutlineGradientBottomColor { get; init; }
-    public UiTextRenderBackend Backend { get; init; }
+    public float FontSize { get; init; } = 12;
+    public float OutlineWidth { get; init; } = 0;
+    public float TexturePadding { get; init; } = 0;
+    public float LineSpacing { get; init; } = 1;
+    public bool Antialias { get; init; } = true;
+    public bool SubpixelText { get; init; } = true;
+    public UiTextStyle Style { get; init; } = UiTextStyle.Regular;
+    public UiTextAlignment Alignment { get; init; } = UiTextAlignment.Left;
+    public Color4 FillColor { get; init; } = Color4.Black;
+    public Color4 OutlineColor { get; init; } = Color4.White;
+    public UiTextGradientMode FillGradientMode { get; init; } = UiTextGradientMode.None;
+    public Color4 FillGradientTopColor { get; init; } = Color4.White;
+    public Color4 FillGradientBottomColor { get; init; } = Color4.White;
+    public UiTextGradientMode OutlineGradientMode { get; init; } = UiTextGradientMode.None;
+    public Color4 OutlineGradientTopColor { get; init; } = Color4.White;
+    public Color4 OutlineGradientBottomColor { get; init; } = Color4.White;
+    public required UiTextRenderBackend Backend { get; init; }
 }
