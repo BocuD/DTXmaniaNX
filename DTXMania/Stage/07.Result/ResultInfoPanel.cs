c494402a3caa6be1c27fb9f56adc2a9e8703bffa
BocuD <duco.born@gmail.com>
2026-05-14 01:06:30 +0900

    Improve results screen fonts

--- DTXMania/Stage/07.Result/ResultInfoPanel.cs
+++ DTXMania/Stage/07.Result/ResultInfoPanel.cs
@@ -23,22 +23,31 @@ public class ResultInfoPanel : UIGroup
         levelLine.position = new Vector3(88, 94, 0);
         levelLine.size = new Vector2(340, 2);
 
-        double level = CDTXMania.chosenChartData.SongInformation.LevelDec[instrument];
+        int DTXLevel;
+        int DTXLevelDeci;
+        if (CDTXMania.DTX.LEVEL[instrument] > 99)
+        {
+            DTXLevel = CDTXMania.DTX.LEVEL[instrument] / 100;
+            DTXLevelDeci = CDTXMania.DTX.LEVEL[instrument] - (DTXLevel * 100);
+        }
+        else
+        {
+            DTXLevel = CDTXMania.DTX.LEVEL[instrument] / 10;
+            DTXLevelDeci = ((CDTXMania.DTX.LEVEL[instrument] - DTXLevel * 10) * 10) + CDTXMania.DTX.LEVELDEC[instrument];
+        }
         
-        int levelIntPart = (int)level;
-        int levelFraction = (int)((level - levelIntPart) * 100);
-        var levelInt = AddChild(new UIText(((int)level) + ".", 68));
-        levelInt.position = new Vector3(295, 108, 0);
+        var levelInt = AddChild(new UIText(DTXLevel.ToString(), 61));
+        levelInt.position = new Vector3(281, 107, 0);
         levelInt.anchor = new Vector2(1, 1);
         levelInt.fontSource = FontSource.System;
-        levelInt.font = "Futura PT Light.otf";
+        levelInt.font = "texgyreadventor-regular.otf";
         levelInt.outlineWidth = 0;
         
-        var levelFractionText = AddChild(new UIText(levelFraction.ToString(), 56));
-        levelFractionText.position = new Vector3(290, 105, 0);
+        var levelFractionText = AddChild(new UIText("." + DTXLevelDeci, 50));
+        levelFractionText.position = new Vector3(278, 102, 0);
         levelFractionText.anchor = new Vector2(0, 1);
         levelFractionText.fontSource = FontSource.System;
-        levelFractionText.font = "Futura PT Light.otf";
+        levelFractionText.font = "texgyreadventor-regular.otf";
         levelFractionText.outlineWidth = 0;
         
         var rateIcon = AddChild(new UIImage(BaseTexture.LoadFromPath(CSkin.Path(@"Graphics\Result\icon_rate.png"))));
@@ -50,18 +59,18 @@ public class ResultInfoPanel : UIGroup
         double rate = stageResult.stPerformanceEntry[instrument].dbPerformanceSkill;
         int rateIntPart = (int)rate;
         int rateFraction = (int)((rate - rateIntPart) * 100);
-        var rateInt = AddChild(new UIText(((int)rate) + ".", 68));
-        rateInt.position = new Vector3(295, 182, 0);
+        var rateInt = AddChild(new UIText(rateIntPart.ToString(), 60));
+        rateInt.position = new Vector3(281, 180, 0);
         rateInt.anchor = new Vector2(1, 1);
         rateInt.fontSource = FontSource.System;
-        rateInt.font = "Futura PT Light.otf";
+        rateInt.font = "texgyreadventor-regular.otf";
         rateInt.outlineWidth = 0;
         
-        var rateFractionText = AddChild(new UIText(rateFraction + "%", 56));
-        rateFractionText.position = new Vector3(290, 179, 0);
+        var rateFractionText = AddChild(new UIText("." + rateFraction + "%", 50));
+        rateFractionText.position = new Vector3(278, 176, 0);
         rateFractionText.anchor = new Vector2(0, 1);
         rateFractionText.fontSource = FontSource.System;
-        rateFractionText.font = "Futura PT Light.otf";
+        rateFractionText.font = "texgyreadventor-regular.otf";
         rateFractionText.outlineWidth = 0;
         
         var skillIcon = AddChild(new UIImage(BaseTexture.LoadFromPath(CSkin.Path(@"Graphics\Result\icon_skill.png"))));
@@ -78,22 +87,22 @@ public class ResultInfoPanel : UIGroup
         double skill = stageResult.stPerformanceEntry[instrument].dbGameSkill;
         int skillIntPart = (int)skill;
         int skillFraction = (int)((skill - skillIntPart) * 100);
-        var skillInt = AddChild(new UIText(((int)skill) + ".", 94));
-        skillInt.position = new Vector3(318, 302, 0);
+        var skillInt = AddChild(new UIText(skillIntPart.ToString(), 82));
+        skillInt.position = new Vector3(315, 299, 0);
         skillInt.anchor = new Vector2(1, 1);
         skillInt.fontSource = FontSource.System;
-        skillInt.font = "Futura PT Medium.otf";
-        skillInt.style = UiTextStyle.Italic;
-        skillInt.texturePadding.X = 10;
+        skillInt.font = "texgyreadventor-italic.otf";
+        skillInt.style = UiTextStyle.Italic | UiTextStyle.Bold;
+        skillInt.texturePadding.X = 50;
         skillInt.outlineWidth = 0;
         
-        var skillFractionText = AddChild(new UIText(skillFraction.ToString("N0"), 60));
-        skillFractionText.position = new Vector3(305, 293, 0);
+        var skillFractionText = AddChild(new UIText("." + skillFraction.ToString("N0"), 53));
+        skillFractionText.position = new Vector3(266, 290, 0);
         skillFractionText.anchor = new Vector2(0, 1);
         skillFractionText.fontSource = FontSource.System;
-        skillFractionText.font = "Futura PT Medium.otf";
-        skillFractionText.style = UiTextStyle.Italic;
-        skillFractionText.texturePadding.X = 10;
+        skillFractionText.font = "texgyreadventor-italic.otf";
+        skillFractionText.style = UiTextStyle.Italic | UiTextStyle.Bold;
+        skillFractionText.texturePadding.X = 50;
         skillFractionText.outlineWidth = 0;
     }
 }
