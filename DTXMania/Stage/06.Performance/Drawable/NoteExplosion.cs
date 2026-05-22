91b0bad9d0c2dc2ef2f331e0a1d012393b19f718
BocuD <duco.born@gmail.com>
2026-05-22 21:22:42 +0900

    Start implementing new note explosions

---
+++ DTXMania/Stage/06.Performance/Drawable/NoteExplosion.cs
@@ -0,0 +1,29 @@
+﻿using DTXMania.Core;
+using DTXMania.UI.Animation;
+using DTXMania.UI.Drawable;
+using DTXMania.UI.Inspector;
+
+namespace DTXMania.Drawable;
+
+public class NoteExplosion : UIGroup
+{
+    [AddChildMenu]
+    public static NoteExplosion Create()
+    {
+        return new NoteExplosion();
+    }
+    
+    public NoteExplosion()
+    {
+        name = $"NoteExplosion";
+
+        animator = new Animator();
+        AnimationClip? loaded = AnimationClipIO.LoadFromFile(CSkin.Path(@"Graphics\Explosion\hit.json"));
+        if (loaded != null)
+        {
+            animator.clips.Add(loaded);
+        }
+
+        isVisible = false;
+    }
+}
